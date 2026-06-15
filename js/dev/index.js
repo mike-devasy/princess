dd0ecef62a4f77ad91bd4d6284f233337f97a9c5
mike.devasy <mike.devasy@gmail.com>
2026-06-15 20:03:59 +0300

    Deploy Princess landing

--- js/dev/index.js
+++ js/dev/index.js
@@ -307,6 +307,35 @@ document.querySelector("[data-fls-popup]") && window.addEventListener("load", ()
 	goHash: false
 } }));
 //#endregion
+//#region src/components/layout/header/plugins/scroll/scroll.js
+function headerScroll() {
+	const header = document.querySelector("[data-fls-header-scroll]");
+	const headerShow = header.hasAttribute("data-fls-header-scroll-show");
+	const headerShowTimer = header.dataset.flsHeaderScrollShow ? header.dataset.flsHeaderScrollShow : 500;
+	const startPoint = header.dataset.flsHeaderScroll ? header.dataset.flsHeaderScroll : 1;
+	let scrollDirection = 0;
+	let timer;
+	document.addEventListener("scroll", function(e) {
+		const scrollTop = window.scrollY;
+		clearTimeout(timer);
+		if (scrollTop >= startPoint) {
+			!header.classList.contains("--header-scroll") && header.classList.add("--header-scroll");
+			if (headerShow) {
+				if (scrollTop > scrollDirection) header.classList.contains("--header-show") && header.classList.remove("--header-show");
+				else !header.classList.contains("--header-show") && header.classList.add("--header-show");
+				timer = setTimeout(() => {
+					!header.classList.contains("--header-show") && header.classList.add("--header-show");
+				}, headerShowTimer);
+			}
+		} else {
+			header.classList.contains("--header-scroll") && header.classList.remove("--header-scroll");
+			if (headerShow) header.classList.contains("--header-show") && header.classList.remove("--header-show");
+		}
+		scrollDirection = scrollTop <= 0 ? 0 : scrollTop;
+	});
+}
+document.querySelector("[data-fls-header-scroll]") && window.addEventListener("load", headerScroll);
+//#endregion
 //#region src/components/pages/home/password-toggle.js
 function initPasswordToggle() {
 	document.querySelectorAll(".field__toggle").forEach((toggle) => {
